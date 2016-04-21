23d0f7376a7667480fa35e04b46d3544a46aea22
sollymay <sollymay@gmail.com>
2016-04-21 15:33:48 -0400

    All of the basic JS challenges completed!

--- Basic-Algorithm-Scripting/test.js
+++ Basic-Algorithm-Scripting/test.js
@@ -189,3 +189,39 @@ function getIndexToIns(arr, num) {
 }
 
 console.log(getIndexToIns([2, 20, 10], 19));
+
+/*One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on. */
+
+function rot13(str) { // LBH QVQ VG!
+  var unshifted =[];
+  for (var i=0; i<str.length; i++){
+    if (str[i].charCodeAt()<65){
+      unshifted.push(String.fromCharCode(str[i].charCodeAt()));
+      console.log(str[i].charCodeAt());
+
+    }
+    else{
+        if (str[i].charCodeAt() - 13 > 65){
+          unshifted.push(String.fromCharCode(str[i].charCodeAt()-13));
+        }
+        else if (str[i] === 'N'){
+          unshifted.push('A');
+        }
+        else {
+          unshifted.push(String.fromCharCode(90 - 13 + (str[i].charCodeAt()- 65)+1));
+
+        }
+
+    }
+  }
+  return unshifted.join('');
+}
+
+// Change the inputs below to test
+console.log(rot13("SERR CVMMN!"));
