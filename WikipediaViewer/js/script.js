b117f87e80a1b6511226b327f058a08bc00eb5d2
sollymay <sollymay@gmail.com>
2016-05-03 19:24:38 -0400

    fixed default hide value for searchbar

--- WikipediaViewer/js/script.js
+++ WikipediaViewer/js/script.js
@@ -1,22 +1,24 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-
+    $('#filter').hide();
     $('#filtersubmit').click(function(e) {
     		e.preventDefault();
     		$('#filter').animate({width: 'toggle'}).focus();
-        var searchTerms = $('#filter').val();
-        if (searchTerms !== '') {
-          $( "#results" ).append( "<p>"+searchTerms+"</p>" );
 
-        }
     });
 
 
+function search(){
+  var searchTerms = $('#filter').val();
+  if (searchTerms !== '') {
+    $( "#results" ).append( "<p>"+searchTerms+"</p>" );
 
+  }
+}
 
     $("#filter").keyup(function (e) {
         if (e.keyCode == 13) {
-            console.log("you pressed enter");
+            search();
         }
     });
 
