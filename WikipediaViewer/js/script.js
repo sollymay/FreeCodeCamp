345c928151ee38fa8cb0ec3d4adeca6fc47c7e16
sollymay <sollymay@gmail.com>
2016-05-03 18:26:07 -0400

    fixed searchbar

--- WikipediaViewer/js/script.js
+++ WikipediaViewer/js/script.js
@@ -1,3 +1,20 @@
-$('#filtersubmit').click(function() {
-    console.log('Searching for '+$('#filter').val());
+$('#filtersubmit').click(function(e) {
+		e.preventDefault();
+		$('#filter').animate({width: 'toggle'}).focus();
+    var searchTerms = $('#filter').val();
+    if (searchTerms !== '') {
+      $( "#results" ).append( "<p>"+searchTerms+"</p>" );
+
+    }
 });
+
+
+
+
+$("#filter").keyup(function (e) {
+    if (e.keyCode == 13) {
+        console.log("you pressed enter");
+    }
+});
+
+console.log(form.elements['filter'].onkeypress);
